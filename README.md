abd461ffc7d661dfd0623c68191d416e599594ef
Nicolas Sénécal <35195832+NicolasSenecal@users.noreply.github.com>
2019-05-06 09:04:52 +0200

    Frist Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# images-captcha-java
+#Images Captcha in Java
 Images selection captcha in JAVA 
+
+##To compile
+```
+javac src/fr/upem/captcha/*.java -d classes 
+```
+##To execute
+```
+java -cp classes fr.umlv.captcha.[path.name]
+```
---
+++ src/fr/upem/captcha/HelloWorld.java
@@ -0,0 +1,19 @@
+package fr.upem.captcha;
+
+/**
+ * @desc Simple program that says "Hello World"
+ * @author Nicolas SENECAL
+ */
+/**
+ * @authors : Olivier MEYER - Nicolas SENECAL
+ * @date : 2019/05/06
+ * @file : HelloWorld.java
+ * @package : fr.upem.captcha
+ */
+
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
